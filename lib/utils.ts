b7586226eca284d14ac4e1825fc2018fcdb6ad87
ghost-sys-coder <juniorbeast177@gmail.com>
2026-01-20 00:16:21 +0300

    Fix several app breakages

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+
 // function to convert a string to ASCII
 export function convertToASCII(inputString: string): string {
   const asciiString = inputString.replace(/[^\x00-\x7F]+/g, "");
---
+++ types/pinecone-doc-splitter.d.ts
@@ -0,0 +1,1 @@
+// declare module '@pinecone-database/doc-splitter';
